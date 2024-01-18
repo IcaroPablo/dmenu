a2ea787129cc89e095363879e0f3b2607ec97da5
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-01-18 20:07:08 -0300

    change colors to orange

--- config.h
+++ config.h
@@ -9,8 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#000000" },
-	[SchemeSel] = { "#bbbbbb", "#005577" },
+	[SchemeNorm] = { "#bbbbbb", "#1d2021" },
+	[SchemeSel] = { "#dddddd", "#BF5700" },
+	/* [SchemeSel] = { "#bbbbbb", "#005577" }, */
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
